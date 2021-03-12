7346d2d98cb63d4ea0272aaaced297ad24802444
JP Memon <johnmemon@gmail.com>
2021-03-11 21:46:27 -0500

    Begin BaseLayer

---
+++ include/BaseLayer.h
@@ -0,0 +1,16 @@
+#ifndef BASELAYER_H
+#define BASELAYER_H
+
+#include <exception.h>
+
+class Layer {
+
+	public:
+		
+		forwardPropagation() { throw std::exception("NotImplementedError"); }
+
+		backPropagation();
+
+}
+
+#endif
